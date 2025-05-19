0ccda83a7b0349ee702d7c76760bc294c3fa4dac
neilk17 <kanakianeil@gmail.com>
2025-05-19 16:19:31 +0530

    install tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./renderer.js",
+    "./index.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: []
+}
