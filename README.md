8bc2b8d6cda7bb06e3c2256cc971319771f9a431
neilk17 <kanakianeil@gmail.com>
2025-05-25 12:13:55 +0530

    fresh install of electron with typescript

--- README.md
+++
@@ -1,5 +0,0 @@
-# write app
-
-**Clone and run for a quick way to see write app in action.**
-
-This is a minimal journaling application.
--- webpack.main.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    mode: process.env.NODE_ENV || 'development',
-    entry: './src/main.js',
-    target: 'electron-main',
-    module: { rules: [] },
-};
