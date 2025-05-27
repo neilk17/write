e48d4f607d849a3c708de507be89bf9d616b37b6
neilk17 <kanakianeil@gmail.com>
2025-05-27 08:11:10 +0530

    upgrade to tailwind 4

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
 module.exports = {
     plugins: [
-        require('tailwindcss'),
-        require('autoprefixer'),
+        require('@tailwindcss/postcss'),
+        // require('tailwindcss'),
+        // require('autoprefixer'),
     ],
 }
